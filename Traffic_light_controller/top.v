09a1407e5fbb6262735f2c37ff9cf988b1d4d2f5
Guruprasanthr <guruprasanthrsh@gmail.com>
2026-03-18 12:57:50 +0530

    Update top.v

--- Traffic_light_controller/top.v
+++ Traffic_light_controller/top.v
@@ -1,1 +1,24 @@
+module top (
+    input  wire clk,
+    input  wire reset,
+    output wire [11:0] lights   // packed: {n_g,n_y,n_r, e_g,e_y,e_r,
+                                //          s_g,s_y,s_r, w_g,w_y,w_r}
+);
+    wire slow_clk;
 
+    clk_divider u_div (
+        .clk_in  (clk),
+        .reset   (reset),
+        .clk_out (slow_clk)
+    );
+
+    traffic_light_4way u_fsm (
+        .clk     (slow_clk),
+        .reset   (reset),
+        .n_green (lights[11]), .n_yellow (lights[10]), .n_red (lights[9]),
+        .e_green (lights[8]),  .e_yellow (lights[7]),  .e_red (lights[6]),
+        .s_green (lights[5]),  .s_yellow (lights[4]),  .s_red (lights[3]),
+        .w_green (lights[2]),  .w_yellow (lights[1]),  .w_red (lights[0])
+    );
+
+endmodule
